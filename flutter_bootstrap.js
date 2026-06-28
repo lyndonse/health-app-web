d40dd021417b314163ec1d950f96b6ef3f5d6d88
lyndonse <chloe79.cha@gmail.com>
2026-06-28 21:09:01 +1200

    fix: trend bar size, lesion count, chart date format

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "664313983"
+    serviceWorkerVersion: "2165896764"
   }
 });
