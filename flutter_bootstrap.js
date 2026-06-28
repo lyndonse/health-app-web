4f8420f1ddfae1e3c0e96f3a835ec8b6db5de273
lyndonse <chloe79.cha@gmail.com>
2026-06-28 22:20:29 +1200

    fix: chart x axis interval + exam list date format

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3603572214"
+    serviceWorkerVersion: "1624910411"
   }
 });
